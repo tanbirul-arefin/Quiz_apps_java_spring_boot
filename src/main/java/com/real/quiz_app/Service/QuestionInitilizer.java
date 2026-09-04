2e3e090f6ca074c6f8fc377ca5a68829ecb04d83
tanbirul-arefin <mdtanbirularefin67@gmail.com>
2026-09-04 17:37:00 +0600

    this system builtup by randomly five question pickup

--- src/main/java/com/real/quiz_app/Service/QuestionInitilizer.java
+++ src/main/java/com/real/quiz_app/Service/QuestionInitilizer.java
@@ -12,76 +12,76 @@ import java.util.List;
 @Service
 
 public class QuestionInitilizer {
-    private List<Question> questionForm;
+    private List<Question> questions;
 
     @PostConstruct
     public void init() {
-        questionForm = new ArrayList<>();
+        questions = new ArrayList<>();
 
-        questionForm.add(new Question(1,
+        questions.add(new Question(1,
                 "How do you create a variable with the numeric value 5?",
                 "num x = 5;",
                 "float x = 5;",
                 "int x = 5;",
                 3, -1));
 
-        questionForm.add(new Question(2,
+        questions.add(new Question(2,
                 "Which data type is used to create a variable that should store text?",
                 "String",
                 "myString",
                 "Txt",
                 1, -1));
 
-        questionForm.add(new Question(3,
+        questions.add(new Question(3,
                 "How do you insert single-line comments in Java?",
                 "// This is a comment",
                 "/* This is a comment",
                 "# This is a comment",
                 1, -1));
 
-        questionForm.add(new Question(4,
+        questions.add(new Question(4,
                 "Which operator is used to add together two values?",
                 "*",
                 "+",
                 "&",
                 2, -1));
 
-        questionForm.add(new Question(5,
+        questions.add(new Question(5,
                 "Which of these is a correct way to create an object in Java?",
                 "MyClass obj = new MyClass();",
                 "class MyClass = new();",
                 "new MyClass obj;",
                 1, -1));
 
-        questionForm.add(new Question(6,
+        questions.add(new Question(6,
                 "What is the size of an int variable in Java?",
                 "8 bit",
                 "32 bit",
                 "16 bit",
                 2, -1));
 
-        questionForm.add(new Question(7,
+        questions.add(new Question(7,
                 "Which keyword is used to return a value inside a method?",
                 "get",
                 "break",
                 "return",
                 3, -1));
 
-        questionForm.add(new Question(8,
+        questions.add(new Question(8,
                 "Java array indexes start with:",
                 "0",
                 "1",
                 "-1",
                 1, -1));
 
-        questionForm.add(new Question(9,
+        questions.add(new Question(9,
                 "Which method can be used to find the length of a string?",
                 "getSize()",
                 "length()",
                 "len()",
                 2, -1));
 
-        questionForm.add(new Question(10,
+        questions.add(new Question(10,
                 "Which keyword is used to inherit a class in Java?",
                 "implements",
                 "extends",
--- src/main/java/com/real/quiz_app/Service/QuizService.java
+++ src/main/java/com/real/quiz_app/Service/QuizService.java
@@ -1,13 +1,41 @@
 package com.real.quiz_app.Service;
 
 import com.real.quiz_app.model.Question;
+import com.real.quiz_app.model.QuestionForm;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @RequiredArgsConstructor
+@Service
 public class QuizService {
 
-    private List<Question> allquestions;
-    private QuestionInitilizer questionInitilizer;
+       private List<Question> allQuestions;
+    private final QuestionInitilizer questionInitilizer;
+
+
+    @PostConstruct
+    public void init() {
+       //allQuestions = QuestionInitilizer.getQuestions();
+    //allQuestions = QuestionInitilizer.getQuestions;
+        allQuestions =questionInitilizer.getQuestions();
+    }
+
+    public QuestionForm getQuestionForm() {
+
+        List<Question> allQuestionscopy = new ArrayList<>();
+        List<Question> selectquestion = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < 5; i++) {
+            int randomNumber = random.nextInt(allQuestions.size());
+            selectquestion.add(allQuestionscopy.get(randomNumber));
+            allQuestionscopy.remove(randomNumber);
+        }
+        return new QuestionForm(selectquestion);
+    }
 }
