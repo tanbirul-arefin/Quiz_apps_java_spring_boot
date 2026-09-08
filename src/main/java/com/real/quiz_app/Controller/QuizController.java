01c04631f54857a9312b0cf90e00c75dd6cb2a70
tanbirul-arefin <mdtanbirularefin67@gmail.com>
2026-09-08 11:42:27 +0600

    add the home page add same coustomize and controller is also added

--- src/main/java/com/real/quiz_app/Controller/QuizController.java
+++ src/main/java/com/real/quiz_app/Controller/QuizController.java
@@ -1,40 +1,40 @@
-package com.real.quiz_app.Controller;
-
-import com.real.quiz_app.model.QuestionForm;
-import com.real.quiz_app.Service.QuizService;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-
-@Controller
-public class QuizController {
-
-    private final QuizService quizService;
-
-    public QuizController(QuizService quizService) {
-        this.quizService = quizService;
-    }
-
-    @GetMapping("/")
-    public String showQuiz(Model model) {
-        model.addAttribute("questionForm", quizService.getQuestionForm());
-        return "quiz";
-    }
-
-    @PostMapping("/result")
-    public String showResult(@ModelAttribute QuestionForm questionForm, Model model) {
-        int totalCorrect = quizService.getResult(questionForm);
-        int totalQuestions = questionForm.getQuestions().size();
-        model.addAttribute("totalCorrect", totalCorrect);
-        model.addAttribute("totalQuestions", totalQuestions);
-        model.addAttribute("questionForm", questionForm);
-        return "result";
-    }
-
-    @GetMapping("/restart")
-    public String restart() {
-        return "redirect:/";
-    }
-}
+//package com.real.quiz_app.Controller;
+//
+//import com.real.quiz_app.model.QuestionForm;
+//import com.real.quiz_app.Service.QuizService;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.PostMapping;
+//
+//@Controller
+//public class QuizController {
+//
+//    private final QuizService quizService;
+//
+//    public QuizController(QuizService quizService) {
+//        this.quizService = quizService;
+//    }
+//
+//    @GetMapping("/")
+//    public String showQuiz(Model model) {
+//        model.addAttribute("questionForm", quizService.getQuestionForm());
+//        return "quiz";
+//    }
+//
+//    @PostMapping("/result")
+//    public String showResult(@ModelAttribute QuestionForm questionForm, Model model) {
+//        int totalCorrect = quizService.getResult(questionForm);
+//        int totalQuestions = questionForm.getQuestions().size();
+//        model.addAttribute("totalCorrect", totalCorrect);
+//        model.addAttribute("totalQuestions", totalQuestions);
+//        model.addAttribute("questionForm", questionForm);
+//        return "result";
+//    }
+//
+//    @GetMapping("/restart")
+//    public String restart() {
+//        return "redirect:/";
+//    }
+//}
---
+++ src/main/java/com/real/quiz_app/Controller/maincontroller.java
@@ -0,0 +1,13 @@
+package com.real.quiz_app.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class maincontroller {
+
+    @GetMapping("/")
+    public String home_page(){
+        return "index";
+    }
+}
